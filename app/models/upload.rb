128986b47b20b709914be67f8a379607962ab2ae
louman <marcus.v.mansur@gmail.com>
2015-09-13 02:35:32 -0300

    validate attachment extension

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,5 +4,6 @@ class Upload < ActiveRecord::Base
 
   has_attached_file :attachment
   validates_attachment_content_type :attachment, content_type: 'application/octet-stream'
+  validates_attachment_file_name :attachment, matches: [/dat\Z/]
 
 end
