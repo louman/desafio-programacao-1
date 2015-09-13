80109d09214b0643d10bf69f059c2820b8f81c7b
louman <marcus.v.mansur@gmail.com>
2015-09-13 02:43:10 -0300

    add upload specs

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Upload, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to belong_to :user }
+
+  it { is_expected.to have_attached_file(:attachment) }
+  it { is_expected.to validate_attachment_content_type(:attachment).allowing('application/octet-stream') }
+  
 end
