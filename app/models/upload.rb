20f7371ea369add41c477acd041d46a0d3f5e3f5
louman <marcus.v.mansur@gmail.com>
2015-09-15 18:11:03 -0300

    add orders_total to upload model

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,9 +1,14 @@
 class Upload < ActiveRecord::Base
 
   belongs_to :user
+  has_many :orders
 
   has_attached_file :attachment
   validates_attachment_content_type :attachment, content_type: 'application/octet-stream'
   validates_attachment_file_name :attachment, matches: [/tab\Z/]
+
+  def orders_total
+    orders.inject(0) { |sum, order| order.total + sum }
+  end
 
 end
--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Upload, type: :model do
+RSpec.describe Upload do
 
-  it { is_expected.to belong_to :user }
+  include_examples 'valid_associations', {
+    have_many: [:orders],
+    belong_to: [:user]
+  }
 
   it { is_expected.to have_attached_file(:attachment) }
   it { is_expected.to validate_attachment_content_type(:attachment).allowing('application/octet-stream') }
-  
+
+  describe '#orders_total' do
+
+    let(:upload) { create(:upload) }
+    let!(:orders) { create_list(:order, 5, upload_id: upload.id) }
+
+    it 'sums orders total values' do
+      expect(upload.orders_total).to eq 50.0
+    end
+
+  end
+
 end
